1312269c0143d2e2b6aad150ce340f1129b5c0f8
Jean <jeanychoi@gmail.com>
2016-01-28 20:11:36 -0800

    HTML CSS JS setup

---
+++ interface.js
@@ -0,0 +1,9 @@
+
+mapboxgl.accessToken = 'pk.eyJ1IjoicGF1bHNjaG9pIiwiYSI6ImNpanltYTR5YzIwamZ2d201eTF6dXd2NGIifQ.VhnYA-RLHhYhw9Fht-HLwA';
+
+var map = new mapboxgl.Map({
+    container: 'map', // container id
+    style: 'mapbox://styles/paulschoi/cijymanoy00hvqtkqsngv391z', //stylesheet location
+    center: [-74.50, 40], // starting position
+    zoom: 9 // starting zoom
+});
